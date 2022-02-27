981b6e00e9f65747962556de35c88c41016c9511
kawpls <jmugaldeb@gmail.com>
2022-02-27 00:32:24 -0600

    canna theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,31 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        canna: {
+          primary: "#365314",
+
+          secondary: "#854d0e",
+
+          accent: "#047857",
+
+          neutral: "#1c1917",
+
+          "base-100": "#272726",
+
+          info: "#84cc16",
+
+          success: "#15803d",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+      "garden",
+      "halloween",
+    ],
+  },
 };
