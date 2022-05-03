35e3310d4bd737d811de9de920e43b23de59dbc7
Manuel <jmugaldeb@gmail.com>
2022-05-03 09:03:40 -0500

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,51 +16,25 @@ module.exports = {
     themes: [
       {
         canna: {
-          primary: "#365314",
+          "primary": "#365314",
 
-          secondary: "#854d0e",
+          "secondary": "#854d0e",
 
-          accent: "#047857",
+          "accent": "#047857",
 
-          neutral: "#1c1917",
+          "neutral": "#1c1917",
 
           "base-100": "#272726",
 
-          info: "#84cc16",
+          "info": "#84cc16",
 
-          success: "#15803d",
+          "success": "#15803d",
 
-          warning: "#FBBD23",
+          "warning": "#FBBD23",
 
-          error: "#F87272",
+          "error": "#F87272",
         },
       },
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
     ],
   },
 };
